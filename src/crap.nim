0f1ee06ea59e007351c89b529e2cc5f6c051522f
Anirudh <icyph0x@pm.me>
2018-11-06 17:04:12 +0530

    Support crapping multiple files
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- src/crap.nim
+++ src/crap.nim
@@ -3,31 +3,32 @@ import
   strformat,
   times
 
-proc trash*(file: string) =
+proc trash*(path: string) =
   var trashHome: string
-  let fullFile = expandFilename(file)
+  let fullPath= expandFilename(path)
 
   if existsEnv("XDG_DATA_HOME"):
     trashHome = joinPath(getEnv("XDG_DATA_HOME"), "Trash")
   else:
     trashHome = joinPath(getHomeDir(), ".local/share/Trash")
-  moveFile(fullFile, fmt("{trashHome}/files/{file}"))
+  moveFile(fullPath, fmt("{trashHome}/files/{path}"))
 
   let 
     t = getTime()
     formattedTime = t.format("yyyy-MM-dd") & "T" & t.format("HH:MM:ss")
 
   var trashInfo = fmt"""[Trash Info]
-Path={fullFile}
+Path={fullPath}
 DeletionDate={formattedTime}"""
 
-  writeFile(joinPath(trashHome, "info", fmt"{file}.info"), trashInfo)
+  writeFile(joinPath(trashHome, "info", fmt"{path}.info"), trashInfo)
 
 when isMainModule:
   var del: string
   try:
-    del = paramStr(1)
-    trash(del)
+    for f in countUp(1, paramCount()):
+      del = paramStr(f)
+      trash(del)
   except IndexError:
     echo("error: no path specified")
     quit(0)
