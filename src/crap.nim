30438d08b8c5f307e18edb1445f12523be07385b
ayko2573 <51141144+ayko2573@users.noreply.github.com>
2020-06-06 12:19:04 +0100

    Save multiple files with the same name (#5)
    * Save multiple files with the same name
    
    If you delete a file with the same name as another file in your trashHome dir crap will simply replace the old file with the one.
    This behaviour isn't what most users would expect.
    This patch allow crap to save multiple files with the same name in the same way that most linux gui file managers deal with the trash.
    
    * Also allow saving multiple dir

--- src/crap.nim
+++ src/crap.nim
@@ -5,15 +5,24 @@ import
   times
 
 proc crap*(path: string) =
-  let fullPath = expandFilename(path)
-  let fname = extractFilename(fullPath)
+  var fullPath = expandFilename(path)
+  var fname = extractFilename(fullPath)
 
   let trashHome = if existsEnv("XDG_DATA_HOME"):
     getEnv("XDG_DATA_HOME") / "Trash"
   else:
     getHomeDir() / ".local/share/Trash"
 
-  moveFile(fullPath, trashHome / "files" / fname)
+  if existsFile(trashHome / "files" / fname) or existsDir(trashHome / "files" / fname):
+    var i = 2
+    while true:
+      if not (existsFile(trashHome / "files" / fname & '.' & $i) or existsDir(trashHome / "files" / fname & '.' & $i) ):
+        fname = fname & '.' & $i
+        moveFile(fullPath, trashHome / "files" / fname)
+        break
+      i = i+1
+  else:
+    moveFile(fullPath, trashHome / "files" / fname)
 
   let 
     t = getTime()
